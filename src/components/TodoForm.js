be292e943565ef8be7ea7d634d3e8acaa7d2684b
Karla Nieves <karlanieves@Karlas-Air.hsd1.ca.comcast.net>
2021-05-03 19:27:31 -0700

    working on the todoform class component

---
+++ src/components/TodoForm.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default class TodoForm extends React.Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
